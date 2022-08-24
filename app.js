dcd288bfbbd03dc173d7bbb48edf01995f554498
Ana Boteleiro <anaboteleiro@gmail.com>
2022-08-24 19:33:13 +0000

    atualização2

--- app.js
+++ app.js
@@ -30,18 +30,29 @@
 // });
 
 
-
 const express = require('express')
+
+const TransacoesRepositorio = require("./transacoes-repositorio")
+
 const app = express()
 const port = 3000
-app.use (express.static(`${__dirname}/public`))
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
+app.get ('/transacoes',(req,res)=>{})
 
-app.get('/ola', (req, res) => {
-  res.send('Hello World!')
+app.post('/transacoes', (req, res) => {
+    const repositorio = new TransacoesRepositorio()
+    const transacoes = repositorio.listarTransacoes()
+    res.send(transacoes)
+})
+
+app.post('/criar-transacao', (req, res) => {
+    const repositorio = new TransacoesRepositorio()
+    const transacao = req.body
+    repositorio.criarTransacao(transacao)
+    res.status(201).send(transacao)
 })
 
 app.listen(port, () => {
-  console.log(`Servidor ouvindo na porta ${port}`)
+    console.log(`Servidor ouvindo na porta ${port}`)
 })
-
-;
